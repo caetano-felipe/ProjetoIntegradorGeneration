16e277673649df5e867393ea201621276be84477
Felipe Borges Caetano <caetano.fbc@gmail.com>
2020-10-30 13:32:26 -0300

    Modify archives

--- Projeto_integrador_mysql_create.sql
+++ Projeto_integrador_mysql_create.sql
@@ -11,19 +11,14 @@ CREATE TABLE `cliente` (
 	`nome_cliente` varchar(255) NOT NULL,
 	`email_cliente` varchar(255) NOT NULL,
 	`senha_cliente` varchar(255) NOT NULL,
-	`endereco_cliente` varchar(255) NOT NULL,
 	PRIMARY KEY (`id`)
 );
 
 CREATE TABLE `tb_prestador_servicos` (
 	`id` INT NOT NULL AUTO_INCREMENT,
 	`nome_prestador_servicos` varchar(255) NOT NULL,
-	`endereco_prestador_servicos` varchar(255) NOT NULL,
 	`tipo_servicos` varchar(255) NOT NULL,
 	`valor_hora_prestador_servicos` DECIMAL(10,2) NOT NULL,
-	`descricao_prestador_servicos` varchar(255) NOT NULL,
-	`email_prestador_servicos` varchar(255) NOT NULL,
-	`senha_prestador_servicos` varchar(255) NOT NULL,
 	`localidade_prestador_servicos` varchar(255) NOT NULL,
 	`categoria_servicos_id` INT NOT NULL,
 	PRIMARY KEY (`id`)
