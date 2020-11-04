a831fb851cc982e4e9eab7ef129d25cccd3b5fd1
Erick Neves <batistasd678@gmail.com>
2020-11-04 10:12:40 -0300

    Base version

---
+++ Back-end/src/main/java/com/performersinc/jobart/JobartApplication.java
@@ -0,0 +1,13 @@
+package com.performersinc.jobart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobartApplication.class, args);
+	}
+
+}
---
+++ Back-end/src/test/java/com/performersinc/jobart/JobartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.performersinc.jobart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
