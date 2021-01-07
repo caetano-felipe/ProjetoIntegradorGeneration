c607679a558fef2d7f18fc1a4417e8fbb5bf7def
Erick Neves <batistasd678@gmail.com>
2021-01-07 10:21:58 -0300

    Changed about and products card style

--- jobart-ui/src/app/home/home.component.ts
+++ jobart-ui/src/app/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AlertService } from './../services/alert.service';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-home',
@@ -8,6 +10,9 @@ import { AlertService } from './../services/alert.service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+
+  faGithub = faGithub;
+  faLinkedin = faLinkedin;
 
   constructor(
     private router: Router,
